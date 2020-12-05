9bf6de36643b531a464221e6e286cbdde7feb127
mateuspntx <mateuspntx@gmail.com>
2020-12-05 01:39:54 -0300

    chore: import AddTransaction component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
