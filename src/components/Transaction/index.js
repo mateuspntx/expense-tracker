d00bc5fa60e5eba0d106df1ec6ee294526a8f532
mateuspntx <mateuspntx@gmail.com>
2020-12-05 02:24:11 -0300

    chore: create Transaction compopnent

---
+++ src/components/Transaction/index.js
@@ -0,0 +1,17 @@
+const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  return (
+    <>
+      <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+        {transaction.text}{' '}
+        <span>
+          {sign}${Math.abs(transaction.amount)}
+        </span>
+        <button className="delete-btn">x</button>
+      </li>
+    </>
+  );
+};
+
+export default Transaction;
