2069c19b14e2660e97799795d847934064b44b9d
mateuspntx <mateuspntx@gmail.com>
2020-12-05 22:55:41 -0300

    chore: create deleteTransaction function

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -15,8 +15,17 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id,
+    });
+  };
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{ transactions: state.transactions, deleteTransaction }}
+    >
       {children}
     </GlobalContext.Provider>
   );
