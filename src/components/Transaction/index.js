31b7537aefd6926cd8bddfa1f992289915f303c9
mateuspntx <mateuspntx@gmail.com>
2020-12-05 22:57:29 -0300

    chore: call deleteTransaction function on click

--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -1,4 +1,9 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../../context/GlobalState';
+
 const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
   const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
@@ -8,7 +13,12 @@ const Transaction = ({ transaction }) => {
         <span>
           {sign}${Math.abs(transaction.amount)}
         </span>
-        <button className="delete-btn">x</button>
+        <button
+          className="delete-btn"
+          onClick={() => deleteTransaction(transaction.id)}
+        >
+          x
+        </button>
       </li>
     </>
   );
