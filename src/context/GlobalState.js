b3401940dcfea6bbd64dcae59964d8595c97a2c7
mateuspntx <mateuspntx@gmail.com>
2020-12-05 23:41:56 -0300

    chore: create addTransaction function

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,12 +2,7 @@ import { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 },
-  ],
+  transactions: [],
 };
 
 export const GlobalContext = createContext(initialState);
@@ -22,9 +17,20 @@ export const GlobalProvider = ({ children }) => {
     });
   };
 
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction,
+    });
+  };
+
   return (
     <GlobalContext.Provider
-      value={{ transactions: state.transactions, deleteTransaction }}
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
     >
       {children}
     </GlobalContext.Provider>
